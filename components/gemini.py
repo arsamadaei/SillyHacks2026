633384e30255042c6a3e953db3d202cb8d1c65e7
s.alissa926@gmail.com <s.alissa926@gmail.com>
2026-04-01 16:26:53 -0700

    Made changes gang

--- components/gemini.py
+++ components/gemini.py
@@ -1,26 +1,23 @@
 import google.generativeai as genai
-from model import load_trained_model, predict
+from inference import load_trained_model, predict
 
 # Configure with your API key
-genai.configure(api_key="AIzaSyCe5CzEInTfVjwEjp3dOCkv_SZAoozVzM8")
+genai.configure(api_key="AIzaSyBfypU6QJLeaw3lfNSr0V8_4FiB5ODVBhw")
 
 # Load your snake model once
 snake_model, species_list = load_trained_model("snake_model.pth")
 
 # Run inference on your image
 species, confidence = predict("test.png", snake_model, species_list)
+print(species)
 
 # Gemini model
-gemini_model = genai.GenerativeModel("gemini-2.0-flash")
+gemini_model = genai.GenerativeModel("gemini-2.5-flash")
 
-# Use the species result in your prompt
-snake = species
 
 response = gemini_model.generate_content(
-    f"Imagine you're the doctor. Your patient just got bit by a {snake} snake. "
-    f"Tell them what a doctor would, "
-    f"about the symptoms, and how to treat."
+    f"Imagine you're the doctor. Your patient just got bit by a {species} snake. Tell them what a doctor would do about the symptoms, and how to treat."
 )
 
-print(f"Snake identified: {snake}")
+print(f"Snake identified: {species}")
 print(response.text)
